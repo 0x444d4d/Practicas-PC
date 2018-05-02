8d7b011829a62091e110d58ad80eb1dc2b82a4e9
StyLerZ <33236426+StyLerZ@users.noreply.github.com>
2018-05-02 18:45:55 +0100

    Finished P4.s

--- 4/prac4.s
+++ 4/prac4.s
@@ -1,18 +1,23 @@
+# vim:ft=mips
+# Practica 4 de PC
+# David Martín Martín. alu0101053318@ull.edu.es
+
 
 m=5
 n=4
 size=4
 
-
 .data
 
-title:    .asciiz "Practica 4 de PC.\n"
-newline:  .asciiz "\n"
+title:       .asciiz "Practica 4 de PC.\n\n"
+perimstr:    .asciiz "El valor del perimetro es: "
+newline:     .asciiz "\n"
+perim:       .word   0
 
-m1:     .word 1, 2, 3, 4, 5
-        .word 1, 2, 3, 4, 5 
-        .word 1, 2, 3, 4, 5
-        .word 1, 2, 3, 4, 5
+m1:          .word   1, 2, 3, 4, 5
+             .word   1, 2, 3, 4, 5 
+             .word   1, 2, 3, 4, 5
+             .word   1, 2, 3, 4, 5
 
 
 .text
@@ -22,6 +27,7 @@ li $v0, 4
 la $a0, title
 syscall
 
+lw $t4 ,perim
 la $t0, m1
 li $t1, m # counter = m;
 
@@ -29,10 +35,12 @@ fila1:
 
   addi $t1, -1 #counter--;
   
-  #Print first element
   li $v0 1
   lw $a0, 0($t0)
   syscall
+
+  lw $t5, 0($t0)	
+  add $t4, $t4, $t5
 
   addi $t0, size  #matrix[++index];
   bne $t1, 1, fila1 # while counter <= m
@@ -43,13 +51,12 @@ fila1:
   li $t1, n
 
 columna2:
-
-  #li $v0, 4
-  #la $a0, newline
-  #syscall
   
   add $t0, $t0, $t3 
   addi $t1, -1
+
+  lw $t5, 0($t0)	
+  add $t4, $t4, $t5
 
   li $v0 1
   lw $a0, 0($t0)
@@ -61,37 +68,50 @@ columna2:
  
 fila2:
 
-  #Print first element
   li $v0 1
   lw $a0, 0($t0)
   syscall
+
+  lw $t5, 0($t0)	
+  add $t4, $t4, $t5
 
   sub $t0, size  #matrix[--index];
   addi $t1, -1 #counter--;
 
   bne $t1, 1, fila2 # while counter <= m
   
-  li $t1, -1
-  mul $t3, $t1, $t3 #$t2 = -1 * 20
   li $t1, n
  
 columna1:
 
-  #li $v0, 4
-  #la $a0, newline
-  #syscall
-  
-  add $t0, $t0, $t3 
+  sub $t0, $t0, $t3 
   addi $t1, -1
 
   li $v0 1
   lw $a0, 0($t0)
   syscall
 
+  lw $t5, 0($t0)	
+  add $t4, $t4, $t5
+
   bne $t1, 1, columna1
 
+#Fin de la impresion del perimetro.
 
-fin:
+  li $v0, 4
+  la $a0, newline
+  syscall
+
+  li $v0, 4
+  la $a0, perimstr
+  syscall
+
+  sw $t4, perim
+
+  li $v0, 1
+  lw $a0, perim
+  syscall
+
+
 li  $v0,10
 syscall
-
